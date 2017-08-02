8d23f6a5fd68fa48ea4cc9ba00cc340066d42024
Eric Anderson <eric429@gmail.com>
2017-08-02 18:54:35 -0500

    get market data from server

--- pages/ListView.vue
+++ pages/ListView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout row-sm column wrap>
-      <v-card hover class="ma-2 market-card">
+      <v-card hover class="ma-2 market-card" v-for="market in markets">
         <v-card-media
           class="white--text"
           height="200px"
@@ -9,7 +9,7 @@
           <v-container fill-height fluid class="scrim">
             <v-layout fill-height>
               <v-flex xs12 align-end flexbox>
-                <span class="title">Minneapolis Farmers Market</span>
+                <span class="title">{{ market.name }}</span>
               </v-flex>
             </v-layout>
           </v-container>
@@ -27,117 +27,32 @@
           </div>
         </v-card-actions>
       </v-card>
-
-      <v-card hover class="ma-2 market-card">
-        <v-card-media
-          class="white--text"
-          height="200px"
-          src="/static/market-images/downtown-market.png"
-        >
-          <v-container fill-height fluid class="scrim">
-            <v-layout fill-height>
-              <v-flex xs12 align-end flexbox>
-                <span class="title">Nicollet Mall Market</span>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-media>
-        <v-card-title>
-          <div>
-            <span> Thursdays 6am - 6pm</span><br>
-            <span class="grey--text">Nicollet Mall downtown</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat>Directions</v-btn>
-          <div class="rating">
-            <v-btn icon>4 <v-icon >star</v-icon></v-btn>
-          </div>
-        </v-card-actions>
-      </v-card>
-
-      <v-card hover class="ma-2 market-card">
-        <v-card-media
-          class="white--text"
-          height="200px"
-          src="/static/market-images/mill-city.png"
-        >
-          <v-container fill-height fluid class="scrim">
-            <v-layout fill-height>
-              <v-flex xs12 align-end flexbox>
-                <span class="title">Mill City Farmers Market</span>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-media>
-        <v-card-title>
-          <div>
-            <span> Saturdays 8am - 1pm</span><br>
-            <span class="grey--text">Near Guthrie and Mill City Museum</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat>Directions</v-btn>
-          <div class="rating">
-            <v-btn icon>4 <v-icon >star</v-icon></v-btn>
-          </div>
-        </v-card-actions>
-      </v-card>
-
-      <v-card hover class="ma-2 market-card">
-        <v-card-media
-          class="white--text"
-          height="200px"
-          src="/static/market-images/night-market.png"
-        >
-          <v-container fill-height fluid class="scrim">
-            <v-layout fill-height>
-              <v-flex xs12 align-end flexbox>
-                <span class="title">Mill City Night Market</span>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-media>
-        <v-card-title>
-          <div>
-            <span> Tuesdays 3:30pm - 7:30pm </span><br>
-            <span class="grey--text">Commons near US Bank Stadium</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat>Directions</v-btn>
-          <div class="rating">
-            <v-btn icon>5 <v-icon >star</v-icon></v-btn>
-          </div>
-        </v-card-actions>
-      </v-card>
-
-      <v-card hover class="ma-2 market-card">
-        <v-card-media
-          class="white--text"
-          height="200px"
-          src="/static/market-images/capella-market.png"
-        >
-          <v-container fill-height fluid class="scrim">
-            <v-layout fill-height>
-              <v-flex xs12 align-end flexbox>
-                <span class="title">Capella Tower Market</span>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-media>
-        <v-card-title>
-          <div>
-            <span> Wednesdays 11am - 2pm</span><br>
-            <span class="grey--text">Downtown in Capella Tower Atrium</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat>Directions</v-btn>
-          <div class="rating">
-            <v-btn icon>4 <v-icon >star</v-icon></v-btn>
-          </div>
-        </v-card-actions>
-      </v-card>
   </v-layout>
 </template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        markets: []
+      }
+    },
+    mounted () {
+      this.getMarkets();
+    },
+    methods: {
+      getMarkets() {
+        axios.get('http://localhost:3000/api/v1/markets')
+          .then(response => {
+              this.markets = response.data;
+              return response;
+          })
+          .catch(error => {
+              console.log(error.message);
+          });
+      }
+    }
+  }
+</script>
