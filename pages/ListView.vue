8cf3cf9d891ee10f2bc93e951bea2734e47c0f04
Eric Anderson <eric429@gmail.com>
2017-08-23 13:29:13 -0500

    add flip side to cards with more info

--- pages/ListView.vue
+++ pages/ListView.vue
@@ -1,33 +1,85 @@
 <template>
-  <v-layout row-sm column wrap child-flex-sm>
+  <v-layout row-sm column wrap>
     <v-flex v-for="market in markets" :key="market.id">
-      <v-card hover fluid class="ma-2" id="market-card">
-        <v-card-media
-          class="white--text"
-          height="200px"
-          :src="market.image"
+      <v-container grid-list-xs>
+        <v-menu
+          transition="slide-y-transition"
+          bottom
+          right
+          full-width
         >
-          <v-container fill-height fluid class="scrim">
-            <v-layout fill-height>
-              <v-flex xs12 align-end flexbox>
-                <span class="title">{{ market.name }}</span>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-media>
-        <v-card-title>
-          <div>
-            <span>{{ getHumanOpenHours(market.hours) }}</span><br>
-            <span class="grey--text">{{ market.description || "Farmer's Market" }}</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat>Directions</v-btn>
-          <div class="rating">
-            <v-btn icon>5 <v-icon >star</v-icon></v-btn>
-          </div>
-        </v-card-actions>
-      </v-card>
+          <v-card tile hover fluid id="market-card" class="md-1" slot="activator">
+            <v-card-media
+              class="white--text"
+              height="200px"
+              :src="market.image"
+            >
+              <v-container fill-height fluid class="scrim">
+                <v-layout fill-height>
+                  <v-flex xs12 align-end flexbox>
+                    <span class="title">{{ market.name }}</span><br>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-media>
+            <v-card-title>
+              <div>
+                <span>{{ getHumanOpenHours(market.hours) }}</span><br>
+                <span class="grey--text">{{ market.description || "Farmer's Market" }}</span>
+              </div>
+              <div class="directions">
+                <a :href="'http://www.google.com/maps/place/' + market.address.latitude + ',' + market.address.longitude"><v-btn icon><v-icon >directions</v-icon></v-btn></a>
+              </div>
+            </v-card-title>
+          </v-card>
+
+          <v-card tile height="274px">
+            <v-list dense>
+              <v-list-tile>
+                <v-list-tile-title>
+                  <v-list-tile-title class="title">{{ market.name }}</v-list-tile-title>
+                </v-list-tile-title>
+                <v-list-action>
+                  <v-icon class="indigo--text">close</v-icon>
+                </v-list-action>
+              </v-list-tile>
+              <v-list-tile v-if="market.url">
+                <v-list-tile-action>
+                  <v-icon class="indigo--text">public</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title class="max-width">{{ market.url }}</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile v-if="market.phone">
+                <v-list-tile-action>
+                  <v-icon class="indigo--text">phone</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title class="max-width">{{ market.phone }}</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile v-if="market.email">
+                <v-list-tile-action>
+                  <v-icon class="indigo--text">mail</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title class="max-width">{{ market.email }}</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile v-if="market.address">
+                <v-list-tile-action>
+                  <v-icon class="indigo--text">location_on</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content class="max-width">
+                  <v-list-tile-title>{{ market.address.street }}</v-list-tile-title>
+                  <v-list-tile-sub-title>{{ market.address.city }}, {{ market.address.state }} {{ market.address.zip }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+          </v-card>
+        </v-menu>
+      </v-container>
     </v-flex>
   </v-layout>
 </template>
--- pages/SeasonView.vue
+++ pages/SeasonView.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-layout column justify-center>
-    <img src="/static/season.png" width="320" alt="What's in Season?" class="mb-5" />
-  </v-layout>
+  <v-container fluid>
+    <img src="/static/season.png" alt="What's in Season?" class="mb-5" />
+  </v-container>
 </template>
